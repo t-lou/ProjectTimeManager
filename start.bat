60858349aadd658a33ec05af8246816d250771d3
Tongxi Lou <tongxi.lou@gmail.com>
2020-10-11 15:51:40 +0200

    refine bat script to allow jar

--- start.bat
+++ start.bat
@@ -1,1 +1,7 @@
-java ProjectTimeManager.Main
+IF EXIST PTM.jar (
+  ECHO PTM.jar exists, will use jar
+  java -jar PTM.jar
+) ELSE (
+  ECHO PTM.jar not exists, will call class directly
+  java ProjectTimeManager.Main
+)
